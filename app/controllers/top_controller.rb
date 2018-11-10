fa1830dc1b01942e0029e4cc50b05c9a550eb201
heavy_stone <>
2018-11-10 22:44:16 +0900

    add pagination on top

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,7 @@ class TopController < ApplicationController
   def index
     @articles = Article.visible.order(released_at: :desc)
     @articles = @articles.open_to_the_public unless current_member
+    @articles = @articles.page(params[:page]).per(5)
   end
 
   def about
