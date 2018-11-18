01f3496af0deb291548734ff5893129e84283d58
heavy_stone <>
2018-11-18 10:26:34 +0900

    add news control by admin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,8 @@ class ArticlesController < ApplicationController
 
   # 記事一覧
   def index
-    @articles = Article.order(released_at: :desc)
+    @articles = Article.visible.order(released_at: :desc)
     @articles = @articles.open_to_the_public unless current_member
-
-    unless current_member&.administrator?
-      @articles = @articles.visible
-    end
-
     @articles = @articles.page(params[:page]).per(5)
   end
 
@@ -17,62 +12,8 @@ class ArticlesController < ApplicationController
   def show
     # 訪問者や一般メンバーが閲覧権限のないページにアクセスすると、
     # 例外ActiveRecord::RecordNotFoundが発生するようにする
-    articles = Article.all
+    articles = Article.visible
     articles = articles.open_to_the_public unless current_member
-
-    unless current_member&.administrator?
-      articles = articles.visible
-    end
-
     @article = articles.find(params[:id])
-  end
-
-  # 新規登録フォーム
-  def new
-    @article = Article.new
-  end
-
-  # 編集フォーム
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  # 新規作成
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      redirect_to @article, notice: "#{@article.title} ニュース記事を登録しました"
-    else
-      render "new"
-    end
-  end
-
-  # 更新
-  def update
-    @article = Article.find(params[:id])
-    @article.assign_attributes(article_params)
-    if @article.save
-      redirect_to @article, notice: "#{@article.title} ニュース記事を更新しました"
-    else
-      render "edit"
-    end
-  end
-
-  # 削除
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to :articles, notice: "#{@article.title} ニュース記事を削除しました"
-  end
-
-  private def article_params
-    params.require(:article).permit(
-      :title,
-      :body,
-      :released_at,
-      :no_expiration,
-      :expired_at,
-      :member_only
-    )
   end
 end
